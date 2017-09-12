fd75ff03c1c3f7a61c350f7a3327d211ca094a6a
Eric Fritz <eric@eric-fritz.com>
2017-09-12 18:54:08 -0500

    Refactor file layout.

--- backoff.go
+++ backoff.go
@@ -1,6 +1,10 @@
 package backoff
 
-import "time"
+import (
+	"math"
+	"math/rand"
+	"time"
+)
 
 type (
 	// Backoff is the interface to a backoff interval generator.
@@ -20,6 +24,18 @@ type (
 		maxInterval time.Duration
 		current     time.Duration
 	}
+
+	exponentialBackoff struct {
+		minInterval time.Duration
+		maxInterval time.Duration
+		multiplier  float64
+		randFactor  float64
+		attempts    uint
+		maxAttempts uint
+	}
+
+	// ExponentialConfigFunc is a function used to initialize a new exponential backoff.
+	ExponentialConfigFunc func(*exponentialBackoff)
 )
 
 // NewZeroBackoff creates a backoff interval generator which always returns
@@ -61,4 +77,83 @@ func (b *linearBackoff) NextInterval() time.Duration {
 	}
 
 	return current
+}
+
+//
+// Exponential
+
+// NewExponentialBackoff creates an exponential backoff interval generator using
+// the given minimum and maximum interval. The base interval is given by the
+// following function where `n` is the number of previous failed attempts in the
+// current sequence.
+//
+//    `MinInterval * Multiplier ^ n`
+//
+// The value returned on each update is given by the following, where base is the
+// value calculated above. A random factor of zero makes the generator deterministic.
+// Some random jitter tends to work well in practice to avoid issues around a  thundering herd.
+//
+//     `min(MaxInterval, base +/- (base * RandFactor))`.
+func NewExponentialBackoff(minInterval, maxInterval time.Duration, configs ...ExponentialConfigFunc) Backoff {
+	backoff := &exponentialBackoff{
+		minInterval: minInterval,
+		maxInterval: maxInterval,
+		multiplier:  2,
+		randFactor:  0,
+		attempts:    0,
+	}
+
+	for _, config := range configs {
+		config(backoff)
+	}
+
+	// Calculate and stash the maximum number of attempts now. This may be
+	// expensive as it involves logs. We need to know the max number of
+	// attempts now so we can shield ourselves from overflow when dealing
+	// with larger intervals.
+
+	// min * mult ^ n <     max
+	//       mult ^ n <     max / min
+	//              n < log(max / min) / log(mult)
+
+	var (
+		num         = math.Log(float64(maxInterval / minInterval))
+		denom       = math.Log(backoff.multiplier)
+		maxAttempts = uint(num / denom)
+	)
+
+	backoff.maxAttempts = maxAttempts
+	return backoff
+}
+
+// WithMultiplier sets the base of the exponential function (default is 2).
+func WithMultiplier(multiplier float64) ExponentialConfigFunc {
+	return func(b *exponentialBackoff) { b.multiplier = multiplier }
+}
+
+// WithRandomFactor sets the random factor (default is 0, no randomness).
+func WithRandomFactor(randFactor float64) ExponentialConfigFunc {
+	return func(b *exponentialBackoff) { b.randFactor = randFactor }
+}
+
+func (b *exponentialBackoff) Reset() {
+	b.attempts = 0
+}
+
+func (b *exponentialBackoff) NextInterval() time.Duration {
+	if b.attempts >= b.maxAttempts {
+		return b.maxInterval
+	}
+
+	n := float64(b.attempts)
+	b.attempts++
+
+	return time.Duration(jitter(float64(b.minInterval)*math.Pow(b.multiplier, n), b.randFactor))
+}
+
+func jitter(value, ratio float64) float64 {
+	min := value - (value * ratio)
+	max := value + (value * ratio)
+
+	return min + (max-min+1)*rand.Float64()
 }
--- backoff_test.go
+++ backoff_test.go
@@ -3,18 +3,32 @@ package backoff
 import (
 	"testing"
 	"time"
+
+	"github.com/aphistic/sweet"
+	"github.com/aphistic/sweet-junit"
+	. "github.com/onsi/gomega"
 )
+
+func TestMain(m *testing.M) {
+	RegisterFailHandler(sweet.GomegaFail)
+
+	sweet.Run(m, func(s *sweet.S) {
+		s.RegisterPlugin(junit.NewPlugin())
+
+		s.AddSuite(&BackoffSuite{})
+	})
+}
 
 type BackoffSuite struct{}
 
-func (s *BackoffSuite) TestZeroBackoff(t *testing.T) {
+func (s *BackoffSuite) TestZeroBackoff(t sweet.T) {
 	b := NewZeroBackoff()
 	testSequence(b, time.Millisecond, []uint{0, 0, 0, 0})
 	b.Reset()
 	testSequence(b, time.Millisecond, []uint{0, 0, 0, 0})
 }
 
-func (s *BackoffSuite) TestConstantBackoff(t *testing.T) {
+func (s *BackoffSuite) TestConstantBackoff(t sweet.T) {
 	b1 := NewConstantBackoff(25 * time.Second)
 	b2 := NewConstantBackoff(50 * time.Minute)
 
@@ -27,10 +41,61 @@ func (s *BackoffSuite) TestConstantBackoff(t *testing.T) {
 	testSequence(b2, time.Minute, []uint{50, 50, 50, 50})
 }
 
-func (s *BackoffSuite) TestLinearBackoffMax(t *testing.T) {
+func (s *BackoffSuite) TestLinearBackoffMax(t sweet.T) {
 	b := NewLinearBackoff(time.Millisecond, time.Millisecond, time.Millisecond*4)
-
 	testSequence(b, time.Millisecond, []uint{1, 2, 3, 4, 4, 4})
 	b.Reset()
 	testSequence(b, time.Millisecond, []uint{1, 2, 3, 4, 4, 4})
+}
+
+func (s *BackoffSuite) TestNonRandom(t sweet.T) {
+	b := NewExponentialBackoff(time.Millisecond, time.Minute)
+	testSequence(b, time.Millisecond, []uint{1, 2, 4, 8, 16, 32})
+	b.Reset()
+	testSequence(b, time.Millisecond, []uint{1, 2, 4, 8, 16, 32})
+}
+
+func (s *BackoffSuite) TestMax(t sweet.T) {
+	b := NewExponentialBackoff(time.Millisecond, time.Millisecond*4)
+	testSequence(b, time.Millisecond, []uint{1, 2, 4, 4, 4, 4})
+	b.Reset()
+	testSequence(b, time.Millisecond, []uint{1, 2, 4, 4, 4, 4})
+}
+
+func (s *BackoffSuite) TestRandomized(t sweet.T) {
+	b := NewExponentialBackoff(
+		time.Millisecond,
+		time.Minute,
+		WithRandomFactor(0.25),
+	)
+
+	testRandomizedSequence(b, time.Millisecond, .25, []uint{1, 2, 4, 8, 16, 32})
+	b.Reset()
+	testRandomizedSequence(b, time.Millisecond, .25, []uint{1, 2, 4, 8, 16, 32})
+}
+
+func (s *BackoffSuite) TestOverflowLimit(t sweet.T) {
+	b := NewExponentialBackoff(time.Millisecond, time.Minute)
+
+	for i := 0; i < 100; i++ {
+		Expect(b.NextInterval()).To(BeNumerically(">=", 1000))
+	}
+}
+
+//
+// Sequence Assertion Helpers
+
+func testSequence(b Backoff, base time.Duration, durations []uint) {
+	testRandomizedSequence(b, base, 0, durations)
+}
+
+func testRandomizedSequence(b Backoff, base time.Duration, ratio float64, durations []uint) {
+	for _, duration := range durations {
+		lo := time.Duration(float64(base) * (1 - ratio) * float64(duration))
+		hi := time.Duration(float64(base) * (1 + ratio) * float64(duration))
+
+		val := b.NextInterval()
+		Expect(val).To(BeNumerically(">=", lo))
+		Expect(val).To(BeNumerically("<=", hi))
+	}
 }
