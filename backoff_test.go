4fbc5fae55ad19e18d08604003d38789810e512f
Kristin Davidson <k@kxd.dev>
2020-11-15 13:39:59 -0600

    Move tests from sweet to testify

--- backoff_test.go
+++ backoff_test.go
@@ -4,104 +4,93 @@ import (
 	"testing"
 	"time"
 
-	"github.com/aphistic/sweet"
-	junit "github.com/aphistic/sweet-junit"
-	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/assert"
 )
-
-func TestMain(m *testing.M) {
-	RegisterFailHandler(sweet.GomegaFail)
-
-	sweet.Run(m, func(s *sweet.S) {
-		s.RegisterPlugin(junit.NewPlugin())
-
-		s.AddSuite(&BackoffSuite{})
-	})
-}
 
 type BackoffSuite struct{}
 
-func (s *BackoffSuite) TestZeroBackoff(t sweet.T) {
+func TestZeroBackoff(t *testing.T) {
 	b := NewZeroBackoff()
-	testSequence(b, time.Millisecond, []uint{0, 0, 0, 0})
+	testSequenceNew(t, b, time.Millisecond, []uint{0, 0, 0, 0})
 	b.Reset()
-	testSequence(b, time.Millisecond, []uint{0, 0, 0, 0})
+	testSequenceNew(t, b, time.Millisecond, []uint{0, 0, 0, 0})
 }
 
-func (s *BackoffSuite) TestConstantBackoff(t sweet.T) {
+func TestConstantBackoff(t *testing.T) {
 	b1 := NewConstantBackoff(25 * time.Second)
 	b2 := NewConstantBackoff(50 * time.Minute)
 
-	testSequence(b1, time.Second, []uint{25, 25, 25, 25})
+	testSequenceNew(t, b1, time.Second, []uint{25, 25, 25, 25})
 	b2.Reset()
-	testSequence(b2, time.Minute, []uint{50, 50, 50, 50})
+	testSequenceNew(t, b2, time.Minute, []uint{50, 50, 50, 50})
 
-	testSequence(b1, time.Second, []uint{25, 25, 25, 25})
+	testSequenceNew(t, b1, time.Second, []uint{25, 25, 25, 25})
 	b1.Reset()
-	testSequence(b2, time.Minute, []uint{50, 50, 50, 50})
+	testSequenceNew(t, b2, time.Minute, []uint{50, 50, 50, 50})
+
 }
 
-func (s *BackoffSuite) TestLinearBackoffMax(t sweet.T) {
-	b := NewLinearBackoff(time.Millisecond, time.Millisecond, time.Millisecond*4)
-	testSequence(b, time.Millisecond, []uint{1, 2, 3, 4, 4, 4})
-	b.Reset()
-	testSequence(b, time.Millisecond, []uint{1, 2, 3, 4, 4, 4})
+func TestLinearBackoff(t *testing.T) {
+	t.Run("max", func(t *testing.T) {
+		b := NewLinearBackoff(time.Millisecond, time.Millisecond, time.Millisecond*4)
+		testSequenceNew(t, b, time.Millisecond, []uint{1, 2, 3, 4, 4, 4})
+		b.Reset()
+		testSequenceNew(t, b, time.Millisecond, []uint{1, 2, 3, 4, 4, 4})
+	})
 }
 
-func (s *BackoffSuite) TestNonRandom(t sweet.T) {
-	b := NewExponentialBackoff(time.Millisecond, time.Minute)
-	testSequence(b, time.Millisecond, []uint{1, 2, 4, 8, 16, 32})
-	b.Reset()
-	testSequence(b, time.Millisecond, []uint{1, 2, 4, 8, 16, 32})
-}
+func TestExponentialBackoff(t *testing.T) {
+	t.Run("non-random", func(t *testing.T) {
+		b := NewExponentialBackoff(time.Millisecond, time.Minute)
+		testSequenceNew(t, b, time.Millisecond, []uint{1, 2, 4, 8, 16, 32})
+		b.Reset()
+		testSequenceNew(t, b, time.Millisecond, []uint{1, 2, 4, 8, 16, 32})
+	})
+	t.Run("zero time minimum", func(t *testing.T) {
+		assert.NotPanics(t, func() {
+			NewExponentialBackoff(0*time.Millisecond, time.Millisecond*4)
+		})
+	})
+	t.Run("max", func(t *testing.T) {
+		b := NewExponentialBackoff(time.Millisecond, time.Millisecond*4)
+		testSequenceNew(t, b, time.Millisecond, []uint{1, 2, 4, 4, 4, 4})
+		b.Reset()
+		testSequenceNew(t, b, time.Millisecond, []uint{1, 2, 4, 4, 4, 4})
+	})
+	t.Run("randomized", func(t *testing.T) {
+		b := NewExponentialBackoff(
+			time.Millisecond,
+			time.Minute,
+			WithRandomFactor(0.25),
+		)
 
-func (s *BackoffSuite) TestZeroTimeMinimum(t sweet.T) {
-	Expect(func() {
-		NewExponentialBackoff(0*time.Millisecond, time.Millisecond*4)
-	}).ToNot(Panic())
-}
+		testRandomizedSequenceNew(t, b, time.Millisecond, .25, []uint{1, 2, 4, 8, 16, 32})
+		b.Reset()
+		testRandomizedSequenceNew(t, b, time.Millisecond, .25, []uint{1, 2, 4, 8, 16, 32})
+	})
+	t.Run("overflow limit", func(t *testing.T) {
+		b := NewExponentialBackoff(time.Millisecond, time.Minute)
 
-func (s *BackoffSuite) TestMax(t sweet.T) {
-	b := NewExponentialBackoff(time.Millisecond, time.Millisecond*4)
-	testSequence(b, time.Millisecond, []uint{1, 2, 4, 4, 4, 4})
-	b.Reset()
-	testSequence(b, time.Millisecond, []uint{1, 2, 4, 4, 4, 4})
-}
-
-func (s *BackoffSuite) TestRandomized(t sweet.T) {
-	b := NewExponentialBackoff(
-		time.Millisecond,
-		time.Minute,
-		WithRandomFactor(0.25),
-	)
-
-	testRandomizedSequence(b, time.Millisecond, .25, []uint{1, 2, 4, 8, 16, 32})
-	b.Reset()
-	testRandomizedSequence(b, time.Millisecond, .25, []uint{1, 2, 4, 8, 16, 32})
-}
-
-func (s *BackoffSuite) TestOverflowLimit(t sweet.T) {
-	b := NewExponentialBackoff(time.Millisecond, time.Minute)
-
-	for i := 0; i < 100; i++ {
-		Expect(b.NextInterval()).To(BeNumerically(">=", 1000))
-	}
+		for i := 0; i < 100; i++ {
+			next := int(b.NextInterval())
+			assert.GreaterOrEqual(t, next, 1000)
+		}
+	})
 }
 
 //
 // Sequence Assertion Helpers
-
-func testSequence(b Backoff, base time.Duration, durations []uint) {
-	testRandomizedSequence(b, base, 0, durations)
+func testSequenceNew(t *testing.T, b Backoff, base time.Duration, durations []uint) {
+	testRandomizedSequenceNew(t, b, base, 0, durations)
 }
 
-func testRandomizedSequence(b Backoff, base time.Duration, ratio float64, durations []uint) {
+func testRandomizedSequenceNew(t *testing.T, b Backoff, base time.Duration, ratio float64, durations []uint) {
 	for _, duration := range durations {
-		lo := time.Duration(float64(base) * (1 - ratio) * float64(duration))
-		hi := time.Duration(float64(base) * (1 + ratio) * float64(duration))
+		lo := int64(float64(base) * (1 - ratio) * float64(duration))
+		hi := int64(float64(base) * (1 + ratio) * float64(duration))
 
-		val := b.NextInterval()
-		Expect(val).To(BeNumerically(">=", lo))
-		Expect(val).To(BeNumerically("<=", hi))
+		val := int64(b.NextInterval())
+		assert.GreaterOrEqual(t, val, lo)
+		assert.LessOrEqual(t, val, hi)
 	}
 }
